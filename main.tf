77ca27d636a477f05953a5ea097aeaa8679cb30d
Jorge Vazquez <jvazquez@enkompass.net>
2023-01-10 02:25:33 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,194 @@
+#VPC Deployment
+resource "aws_vpc" "workspaces-vpc" {
+ cidr_block = "10.0.0.0/16"
+ tags = {
+   Name = "workspaces-vpc"
+   "Deployed By" = "Enkompass"
+ }
+}
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.workspaces-vpc.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+   "Deployed By" = "Enkompass"
+ }
+}
+
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.workspaces-vpc.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+   "Deployed By" = "Enkompass"
+ }
+}
+
+resource "aws_internet_gateway" "igw" {
+ vpc_id = aws_vpc.workspaces-vpc.id
+ tags = {
+   Name = "Workspaces VPC IGW"
+   "Deployed By" = "Enkompass"
+ }
+}
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "workspaces-nat" {
+  vpc = true
+  tags = {
+    Name = "Workspaces NAT IP"
+    "Deployed By" = "Enkompass"
+  }
+}
+
+
+# NAT Gateway
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.workspaces-nat.id
+  subnet_id = aws_subnet.public_subnets[1].id
+  tags = {
+    Name = "Workspaces VPC NAT GW"
+    "Deployed By" = "Enkompass"
+ }
+ depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "public_rt" {
+ vpc_id = aws_vpc.workspaces-vpc.id
+
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.igw.id
+ }
+
+ tags = {
+   Name = "Public Subnets"
+   "Deployed By" = "Enkompass"
+ }
+}
+
+resource "aws_route_table" "private_rt" {
+ vpc_id = aws_vpc.workspaces-vpc.id
+
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_nat_gateway.natgw.id
+ }
+
+ tags = {
+   Name = "Public Subnets"
+   "Deployed By" = "Enkompass"
+ }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id = element(aws_subnet.private_subnets[*].id, count.index)
+ route_table_id = aws_route_table.private_rt.id
+}
+
+
+# AWS Directory Service Directory
+resource "aws_directory_service_directory" "simple_ad_directory" {
+  name     = "hubcitytaxservice.local"
+  password = "P@$$word123"
+  size     = "Small"
+  vpc_settings {
+    vpc_id = aws_vpc.workspaces-vpc.id
+    subnet_ids = [ aws_subnet.private_subnets[0].id, aws_subnet.private_subnets[2].id]
+  }
+  tags = {
+    Name = "SimpleAD"
+    Environment = "Production"
+    "Deployed By" = "Enkompass"
+  }
+}
+
+# IAM Trust Relationship
+data "aws_iam_policy_document" "workspaces" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["workspaces.amazonaws.com"]
+    }
+  }
+}
+
+# IAM Roles and Policies
+resource "aws_iam_role" "workspaces-default" {
+  name = "workspaces_DefaultRole"
+  assume_role_policy = data.aws_iam_policy_document.workspaces.json
+}
+
+resource "aws_iam_role_policy_attachment" "workspaces-default-service-access" {
+  role = aws_iam_role.workspaces-default.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonWorkSpacesServiceAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "workspaces-default-self-service-access" {
+  role = aws_iam_role.workspaces-default.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonWorkSpacesSelfServiceAccess"
+}
+
+# Workspaces Directory Attachment
+resource "aws_workspaces_directory" "workspaces-directory" {
+ directory_id = aws_directory_service_directory.simple_ad_directory.id
+  subnet_ids   = [ aws_subnet.private_subnets[0].id, aws_subnet.private_subnets[2].id]
+  depends_on = [aws_iam_role.workspaces-default]
+}
+
+# Windows Standard Bundle
+data "aws_workspaces_bundle" "standard_windows" {
+ bundle_id = "wsb-gk1wpk43z"
+}
+
+# KMS Key
+resource "aws_kms_key" "workspaces-kms" {
+  description = "Workspaces KMS"
+  deletion_window_in_days = 7
+}
+
+# Workspace Deploy
+resource "aws_workspaces_workspace" "workspaces" {
+  directory_id = aws_workspaces_directory.workspaces-directory.id
+  bundle_id = data.aws_workspaces_bundle.standard_windows.id
+
+  # Administrator for SimpleAD
+  # Admin for ManagedAD
+  user_name = "Administrator"
+  root_volume_encryption_enabled = true
+  user_volume_encryption_enabled = true
+  volume_encryption_key = aws_kms_key.workspaces-kms.arn
+  workspace_properties {
+    compute_type_name = "STANDARD"
+    user_volume_size_gib = 50
+    root_volume_size_gib = 80
+    running_mode = "ALWAYS_ON"
+    running_mode_auto_stop_timeout_in_minutes = 60
+  }
+  tags = {
+    Name = "Setup Workspace"
+    Environment = "Production"
+    "Deployed By" = "Enkompass"
+  }
+
+  depends_on = [
+    aws_iam_role.workspaces-default,
+    aws_workspaces_directory.workspaces-directory
+  ]
+}
+
---
+++ variables.tf
@@ -0,0 +1,41 @@
+### TERRAFORM VARIABLES
+
+# PROVIDER
+# AWS Region
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-1"
+}
+
+variable "akid" {
+  description = "AWS Key"
+  default     = ""
+}
+
+variable "secret" {
+  description = "AWS Secret"
+  default     = ""
+}
+
+
+# SUBNETS
+# Public Subnet CIDRs
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+# Private Subnet CIDRs
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+# Availability Zones
+variable "azs" {
+ type        = list(string)
+ description = "Availability Zones"
+ default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
